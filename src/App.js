5c8fa3a6c3d7ee3eaa9082db98dd23f326640c85
Hassan <157310261+mhassaniqbalsial@users.noreply.github.com>
2025-08-22 22:09:36 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import Portfolio from './components/Portfolio';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Header />
       <main className="main-content">
         <Hero />
-        <Portfolio />
       </main>
     </div>
   );
